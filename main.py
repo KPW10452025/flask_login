fc3601f8c939b5f7027a14724e598d4269add774
PoWei <10452025@mail.ntou.edu.tw>
2021-08-12 18:38:38 +0900

    Add two route:route("/") and route("logout")

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from flask import Flask
 # pip install flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 # pip install flask_login
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 # https://flask-login.readthedocs.io/en/latest/
+# https://hackmd.io/@shaoeChen/ryvr_ly8f?type=view#note_1
 
 import os
 # pip install python-dotenv
@@ -37,3 +38,37 @@ class User(db.Model, UserMixin):
 # sqlite> .table
 # user
 # sqlite> .quit
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+@app.route("/")
+def index():
+	user01 = User.query.filter_by(username = 'Webb').first()
+	# user01 = User.query.filter_by(username = 'Tom').first()
+	if user01:
+		login_user(user01)
+		return "You are now logged in!"
+	else:
+		return "You need to log in!"
+
+# Add a data into database
+# Open Python3 Shell
+# >>> from main import db, User
+# >>> user1=User(username='Webb')
+# >>> db.session.add(user1)
+# >>> db.session.commit()
+# Check it.
+# >>> result=User.query.all()
+# >>> result[0].username
+# 'Webb'
+
+@app.route("/logout")
+@login_required
+def logout():
+	logout_user()
+	return "You are now logged out!"
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = 3000)
